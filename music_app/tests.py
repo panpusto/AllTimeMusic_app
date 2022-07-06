5e164ee8adfc0e45c4886f26f28cd5b5fc101eca
Cezary <102999499+panpusto@users.noreply.github.com>
2022-07-06 14:37:50 +0200

    more tests added

--- music_app/tests.py
+++ music_app/tests.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-
 import pytest
 from django.contrib.auth.models import User
 from django.test import Client
-from music_app.models import Genre, Band, Label, Album
+from music_app.models import Genre, Band, Label, Album, Musician, MusicianBand
 
 
 @pytest.fixture
@@ -77,6 +75,30 @@ def album(user, band, genre, label):
     return album
 
 
+@pytest.fixture
+def musician(user):
+    musician = Musician.objects.create(
+        name="Nergal",
+        full_name="Adam Darski",
+        place_of_birth='Danzig, Poland',
+        bio='founder of Behemoth',
+        added_by_id=user.id
+    )
+    return musician
+
+
+@pytest.fixture
+def musician_to_band(musician, band):
+    musician_to_band = MusicianBand.objects.create(
+        musician=musician,
+        band=band,
+        year_from=1999,
+        year_to=2010,
+        role='leading guitar, vocal'
+    )
+    return musician_to_band
+
+
 @pytest.mark.django_db
 def test_connection(client):
     response = client.get('')
@@ -121,7 +143,7 @@ def test_bands_by_genre_list_view(client):
 
 @pytest.mark.django_db
 def test_band_details_view(client, band, label, genre, user):
-    response = client.get('/band/details/1/')
+    response = client.get(f'/band/details/{band.id}/')
     assert response.context.get('name' == 'Iron Maiden')
     assert response.context.get('country_of_origin' == 'Great Britain')
     assert response.context.get('location' == 'Leyton, London')
@@ -142,10 +164,8 @@ def test_band_create_view(client, label, genre, user):
     client.force_login(user)
     get_response = client.get('/band/create/')
     assert get_response.status_code == 200  # for logged user
-    count_before_create = Band.objects.count()
-    post_response = client.post(
-        '/band/create/',
-        {
+    # count_before_create = Band.objects.count()
+    new_band = {
             'name': 'Metallica',
             'country_of_origin': 'USA',
             'location': 'San Francisco',
@@ -156,10 +176,13 @@ def test_band_create_view(client, label, genre, user):
             'current_label': label.id,
             'bio': 'Thrash Metal Titans',
             'added_by_id': user.id
-        },
+        }
+    post_response = client.post(
+        '/band/create/',
+        new_band,
         follow=True
     )
-    count_after_create = Band.objects.count()
+    # count_after_create = Band.objects.count()
     assert post_response.status_code == 200
     # assert count_after_create == count_before_create + 1  # TODO doesn't work in terminal, test passed with 'run'
 
@@ -193,7 +216,7 @@ def test_delete_band_view(client, band, user):
 
 @pytest.mark.django_db
 def test_album_details_view(client, album, band, label, genre, user):
-    response = client.get('/album/details/1/')
+    response = client.get(f'/album/details/{album.id}/')
     assert response.context.get('title' == 'Black Album')
     assert response.context.get('band' == band.id)
     assert response.context.get('genre' == genre)
@@ -207,13 +230,13 @@ def test_album_details_view(client, album, band, label, genre, user):
 
 
 @pytest.mark.django_db
-def test_album_create_view(client, band, label, genre, user):
+def test_album_create_view(client, album, band, label, genre, user):
     response = client.get('/album/create/')
     assert response.status_code == 302  # for not logged user
     client.force_login(user)
     get_response = client.get('/album/create/')
     assert get_response.status_code == 200
-    count_before_create = Album.objects.count()
+    # count_before_create = Album.objects.count()
     new_band = {
             'title': 'Black Album',
             'band': band.id,
@@ -229,6 +252,189 @@ def test_album_create_view(client, band, label, genre, user):
         new_band,
         follow=True,
     )
-    count_after_create = Album.objects.count()
+    # count_after_create = Album.objects.count()
+    assert post_response.status_code == 200
+    # assert count_after_create == count_before_create + 1    # TODO doesn't work in terminal, test passed with 'run'
+
+
+@pytest.mark.django_db
+def test_album_update_view(client, album, band, label, genre, user):
+    album = Album.objects.first()
+    response = client.get(f'/album/update/{album.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/album/update/{album.id}/')
+    assert get_response.status_code == 200
+    album.title = 'Master of Puppets'
+    album.format = 3
+    album.save()
+    assert get_response.status_code == 200
+    album_obj = Album.objects.get(id=album.id)
+    assert album_obj.title == album.title
+    assert album_obj.format == album.format
+
+
+@pytest.mark.django_db
+def test_album_delete_view(client, album, user):
+    album = Album.objects.first()
+    response = client.get(f'/album/delete/{album.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/album/delete/{album.id}/')
+    assert get_response.status_code == 302
+    album_ids = [album.id for album in Album.objects.all()]
+    assert album.id not in album_ids
+
+
+@pytest.mark.django_db
+def test_musician_details_view(client, musician, user, band, musician_to_band):
+    response = client.get(f'/musician/details/{musician.id}/')
+    assert response.context.get('name' == 'Nergal')
+    assert response.context.get('full_name' == 'Adam Darski')
+    assert response.context.get('place_of_birth' == 'Danzig, Poland')
+    assert response.context.get('bio' == 'founder of Behemoth')
+    assert response.context.get('added_by_id' == user)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_musician_create_view(client, user, band, musician, musician_to_band):
+    response = client.get('/musician/create/')
+    assert response.status_code == 302  # for not logged user
+    client.force_login(user)
+    get_response = client.get('/musician/create/')
+    assert get_response.status_code == 200
+    count_before_create = Musician.objects.count()
+    new_musician = {
+        'name': 'Joe',
+        'full_name': 'Joe Talbot',
+        'place_of_birth': 'Exeter, England',
+        'bio': 'Welsh singer and songwriter. Vocalist for Idles',
+        'added_by_id': user.id
+    }
+    post_response = client.post(
+        '/musician/create/',
+        new_musician,
+        follow=True,
+    )
+    count_after_create = Musician.objects.count()
     assert post_response.status_code == 200
     assert count_after_create == count_before_create + 1
+
+
+@pytest.mark.django_db
+def test_musician_update_view(client, user, band, musician, musician_to_band):
+    musician = Musician.objects.first()
+    response = client.get(f'/musician/update/{musician.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/musician/update/{musician.id}/')
+    assert get_response.status_code == 200
+    musician.name = 'Orion'
+    musician.full_name = 'Tomasz Wróblewski'
+    musician.save()
+    assert get_response.status_code == 200
+    musician_obj = Musician.objects.get(id=musician.id)
+    assert musician_obj.name == musician.name
+    assert musician_obj.full_name == musician.full_name
+
+
+@pytest.mark.django_db
+def test_musician_delete_view(client, musician, user):
+    musician = Musician.objects.first()
+    response = client.get(f'/musician/delete/{musician.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/musician/delete/{musician.id}/')
+    assert get_response.status_code == 302
+    musician_ids = [musician.id for musician in Musician.objects.all()]
+    assert musician.id not in musician_ids
+
+
+@pytest.mark.django_db
+def test_label_details_view(client, user, label):
+    response = client.get(f'/label/details/{label.id}/')
+    assert response.context.get('name' == 'Sony Music Polska')
+    assert response.context.get('address' == 'ul.Zajęcza, Warsaw')
+    assert response.context.get('country' == 'Poland')
+    assert response.context.get('status' == 1)
+    assert response.context.get('styles' == 'pop')
+    assert response.context.get('founding_year' == 1995)
+    assert response.context.get('added_by_id' == user)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_label_create_view(client, user, label):
+    response = client.get('/label/create/')
+    assert response.status_code == 302  # for not logged user
+    client.force_login(user)
+    get_response = client.get('/label/create/')
+    assert get_response.status_code == 200
+    count_before_create = Label.objects.count()
+    new_label = {
+        'name': 'New Aeon Musick',
+        'address': 'Danzig 4324',
+        'country': 'Poland',
+        'status': 1,
+        'styles': 'blackened-death metal, black metal',
+        'founding_year': '2006',
+        'added_by_id': user.id
+    }
+    post_response = client.post(
+        '/label/create/',
+        new_label,
+        follow=True,
+    )
+    count_after_create = Label.objects.count()
+    assert post_response.status_code == 200
+    assert count_after_create == count_before_create + 1
+
+
+@pytest.mark.django_db
+def test_label_update_view(client, user, label):
+    label = Label.objects.first()
+    response = client.get(f'/label/update/{label.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/label/update/{label.id}/')
+    assert get_response.status_code == 200
+    label.name = 'Astigmatic Records'
+    label.styles = 'avant-garde'
+    label.save()
+    assert get_response.status_code == 200
+    label_obj = Label.objects.get(id=label.id)
+    assert label_obj.name == label.name
+    assert label_obj.styles == label.styles
+
+
+@pytest.mark.django_db
+def test_label_delete_view(client, label, user):
+    label = Label.objects.first()
+    response = client.get(f'/label/delete/{label.id}/')
+    assert response.status_code == 302
+    client.force_login(user)
+    get_response = client.get(f'/label/delete/{label.id}/')
+    assert get_response.status_code == 302
+    label_ids = [label.id for label in Label.objects.all()]
+    assert label.id not in label_ids
+
+
+@pytest.mark.django_db
+def test_genre_details_view(client, genre):
+    pass
+
+
+@pytest.mark.django_db
+def test_genre_create_view(client, genre):
+    pass
+
+
+@pytest.mark.django_db
+def test_genre_update_view(client, genre):
+    pass
+
+
+@pytest.mark.django_db
+def test_genre_delete_view(client, genre):
+    pass
