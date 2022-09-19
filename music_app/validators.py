40cbd0d7ec4c584d54e6f0604ccd800a77d4b61a
Cezary <102999499+panpusto@users.noreply.github.com>
2022-09-19 23:00:44 +0200

    update password validator

--- music_app/validators.py
+++ music_app/validators.py
@@ -2,9 +2,15 @@ from django.core.exceptions import ValidationError
 
 
 def validate_password(param):
+    special_symbols = ['!', '@', '#', '%']
+
     if len(param) < 8:
         raise ValidationError("Password should be at least 8 characters.")
     elif len(param) > 20:
         raise ValidationError("Password should be no more than 20 characters.")
     elif not any(char.isdigit() for char in param):
         raise ValidationError("Password should have at least one number.")
+    elif not any(char.islower() for char in param):
+        raise ValidationError("Password should have at least one lower case letter.")
+    elif not any(char in special_symbols for char in param):
+        raise ValidationError("Password should have at least on of these symbols: !, @, #, %.")
